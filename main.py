c9b4e7a78f4bfe95276a22cb75c149863ae40a30
orsaar0 <orsaar0@gmail.com>
2022-03-09 19:29:59 +0200

    hebrew letters in menu

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def main():
     label.place(x=0, y=0)
 
     options = {
-        "Alef": "Alef_bet_images/Alef.jpg", "Bet": "Alef_bet_images/Bet.jpg", "Gimel": "Alef_bet_images/Gimel.jpg",
-        "Daled": "Alef_bet_images/Daled.jpg", "Hey": "Alef_bet_images/Hey.jpg"
-        , "Vav": "Alef_bet_images/Vav.jpg", "Zain": "Alef_bet_images/Zain.jpg", "Het": "Alef_bet_images/Het.jpg",
-        "Tet": "Alef_bet_images/Tet.jpg"
-        , "Yud": "Alef_bet_images/Yud.jpg", "Kaf": "Alef_bet_images/Kaf.jpg", "Lamed": "Alef_bet_images/Lamed.jpg"
-        , "Mem": "Alef_bet_images/Mem.jpg", "Nun": "Alef_bet_images/Nun.jpg",
-        "Sameh": "Alef_bet_images/Sameh.jpg", "Haayin": "Alef_bet_images/Haayin.jpg", "Pey": "Alef_bet_images/Pey.jpg"
-        , "Tzadik": "Alef_bet_images/Tzadik.jpg", "Kuf": "Alef_bet_images/Kuf.jpg",
-        "Reish": "Alef_bet_images/Reish.jpg",
-        "Shin": "Alef_bet_images/Shin.jpg", "Taf": "Alef_bet_images/Taf.jpg"}
+        "א": "Alef_bet_images/Alef.jpg", "ב": "Alef_bet_images/Bet.jpg", "ג": "Alef_bet_images/Gimel.jpg",
+        "ד": "Alef_bet_images/Daled.jpg", "ה": "Alef_bet_images/Hey.jpg"
+        , "ו": "Alef_bet_images/Vav.jpg", "ז": "Alef_bet_images/Zain.jpg", "ח": "Alef_bet_images/Het.jpg",
+        "ט": "Alef_bet_images/Tet.jpg"
+        , "י": "Alef_bet_images/Yud.jpg", "כ": "Alef_bet_images/Kaf.jpg", "ל": "Alef_bet_images/Lamed.jpg"
+        , "מ": "Alef_bet_images/Mem.jpg", "נ": "Alef_bet_images/Nun.jpg",
+        "ס": "Alef_bet_images/Sameh.jpg", "ע": "Alef_bet_images/Haayin.jpg", "פ": "Alef_bet_images/Pey.jpg"
+        , "צ": "Alef_bet_images/Tzadik.jpg", "ק": "Alef_bet_images/Kuf.jpg",
+        "ר": "Alef_bet_images/Reish.jpg",
+        "ש": "Alef_bet_images/Shin.jpg", "ת": "Alef_bet_images/Taf.jpg"}
 
     clicked = tk.StringVar(root)
     clicked.set("Click To Pick")
